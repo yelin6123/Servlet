7cc0ac3ddee929696978029c3954735da6f485eb
학원 <test@test.com>
2023-09-04 21:31:47 +0900

    노트북에서 변경을 일으켰다.

--- src/main/java/com/test/lesson01/GetMethodQuiz07.java
+++ src/main/java/com/test/lesson01/GetMethodQuiz07.java
@@ -13,6 +13,8 @@ public class GetMethodQuiz07 extends HttpServlet {
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// add comment
+		
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/html");
 		
